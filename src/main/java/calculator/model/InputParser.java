ed90c5616309280502d5148bc8903d92f35cad87
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-17 04:25:01 +0900

    feat(input parser): 입력 문자열 분할
    입력된 문자열의 숫자를 구분자로 분할하여 리스트에 저장합니다.
    
    - 커스텀 구분자와 기본 구분자를 이용해 숫자들을 분리합니다.

---
+++ src/main/java/calculator/model/InputParser.java
@@ -0,0 +1,25 @@
+package calculator.model;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class InputParser {
+    private final Validator validator = new Validator();
+    private final CustomDelimiter customDelimiter = new CustomDelimiter();
+
+    public List<BigDecimal> parse(String text) {
+        // 1. 커스텀 구분자가 있는지 확인하고, 있다면 구분자와 숫자 문자열 분리
+        String[] parts = customDelimiter.separate(text);
+        String delimiter = parts[0];
+        String numbersText = parts[1];
+
+        // 2. 구분자를 기준으로 문자열을 잘라 숫자 문자열 배열 생성
+        String[] stringNumbers = numbersText.split(delimiter);
+
+        return Arrays.stream(stringNumbers)
+                .map(validator::validateAndParse)
+                .collect(Collectors.toList());
+    }
+}
