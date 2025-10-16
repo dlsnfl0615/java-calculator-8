58ba81bf6b58b2adca198cfc87790c069cd6b949
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-17 05:12:12 +0900

    refactor: 에러 메세지 출력

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,7 +1,13 @@
 package calculator.view;
 
+import java.math.BigDecimal;
+
 public class OutputView {
-    public void printResult(int result) {
+    public void printResult(BigDecimal result) {
         System.out.println("결과 : " + result);
+    }
+
+    public void printError(String message) {
+        System.out.println("[ERROR] " + message);
     }
 }
