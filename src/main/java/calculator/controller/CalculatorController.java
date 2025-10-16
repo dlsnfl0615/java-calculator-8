3f8dd67644b99cd3ced87688bb2335fdd9033dfc
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-17 05:12:34 +0900

    feat: MVC 계층 조합해서 최종 결과 출력

---
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -0,0 +1,35 @@
+package calculator.controller;
+import calculator.model.Calculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
+import java.math.BigDecimal;
+
+public class CalculatorController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final Calculator calculator;
+
+    public CalculatorController(InputView inputView, OutputView outputView, Calculator calculator) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.calculator = calculator;
+    }
+
+    public void run() {
+        try {
+            // 1. View를 통해 사용자 입력 받기
+            String expression = inputView.readExpression();
+
+            // 2. Model에게 계산 요청하기
+            BigDecimal result = calculator.add(expression);
+
+            // 3. View를 통해 결과 출력하기
+            outputView.printResult(result);
+
+        } catch (IllegalArgumentException e) {
+            // 4. Model에서 예외 발생 시 View를 통해 에러 메시지 출력하기
+            outputView.printError(e.getMessage());
+        }
+    }
+}
