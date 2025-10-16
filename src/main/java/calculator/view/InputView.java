396901e709b35b6480cb6c26d8d8dbb8a111ad6e
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-17 05:11:53 +0900

    fix: 커스텀 구분자를 위해 입력하는 엔터키가 입력을 끝내버리는 로직 수정

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -5,6 +5,12 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     public String readExpression() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        return Console.readLine();
+        String firstInput = Console.readLine();
+        if (firstInput.contains("/")) {
+            String secondInput = Console.readLine();
+            return firstInput + "\n" + secondInput;
+        }
+
+        return firstInput;
     }
 }
