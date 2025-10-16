ae2a9841f659daa32e4dc2fa8b001d0ff29c7822
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-16 23:59:13 +0900

    feat(custom delimiter): 커스텀 구분자 검증
    입력된 문자열이 기본 구분자로만 이루어 졌는지,
    커스텀 구분자가 입력됐다면 올바른 양식으로 입력됐는지를 검증합니다.
    
    - 커스텀 구분자로 하나의 문자, 두 개 이상의 문자를 입력받는 상황을 검증합니다.
    - 커스텀 구분자의 양식이 올바르지 않은 경우 예외 처리 합니다.
    
    관련 단위 테스트에서 해당 내용을 테스트합니다.
    기능 구현에 맞춰 README.md의 체크박스 업데이트 했습니다.

---
+++ src/main/java/calculator/model/CustomDelimiter.java
@@ -0,0 +1,36 @@
+package calculator.model;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CustomDelimiter {
+    // 기본 구분자: 쉼표 또는 콜론
+    private static final String DEFAULT_DELIMITER = ",|:";
+    // 커스텀 구분자 패턴: "//(.)\n(.*)"
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.+)\n(.*)");
+
+    // 입력 문자열에서 [구분자, 숫자부분] 배열을 추출하여 반환
+    public String[] separate(String text) {
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(text);
+        boolean isPerfectMatch = matcher.find();
+
+        // 커스텀 구분자 사용을 위해 슬래시와 엔터를 입력했지만 올바른 양식이 아닌 경우
+        boolean hasAttemptSigns = text.startsWith("/") || text.contains("\n");
+        if (hasAttemptSigns && !isPerfectMatch) {
+            throw new IllegalArgumentException("커스텀 구분자 형식 오류: '//[구분자]\\n[숫자]' 형식이어야 합니다.");
+        }
+
+        // 올바른 양식이지만 커스텀 구분자로 마침표를 사용한 경우
+        if (isPerfectMatch && matcher.group(1).equals(".")) {
+            throw new IllegalArgumentException("커스텀 구분자 형식 오류: 커스텀 구분자로 마침표를 사용할 수 없습니다.");
+        }
+
+        // 올바른 커스텀 구분자인 경우
+        if (isPerfectMatch) {
+            return new String[]{matcher.group(1), matcher.group(2)};
+        }
+
+        // 기본 구분자를 사용한 경우
+        return new String[]{DEFAULT_DELIMITER, text};
+    }
+}
---
+++ src/test/java/calculator/model/CustomDelimiterTest.java
@@ -0,0 +1,98 @@
+package calculator.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CustomDelimiterTest {
+    @Test
+    @DisplayName("하나의 문자를 가지는 커스텀 구분자")
+    void add_with_single_char_delimiter() {
+        CustomDelimiter delimiter = new CustomDelimiter();
+        String[] result = delimiter.separate("//a\n1,2.3a4");
+        String customDelimiter = result[0];
+        String numbers = result[1];
+        Assertions.assertThat(customDelimiter).isEqualTo("a");
+        Assertions.assertThat(numbers).isEqualTo("1,2.3a4");
+    }
+
+    @Test
+    @DisplayName("여러개의 문자를 가지는 커스텀 구분자")
+    void add_with_plural_char_delimiter() {
+        CustomDelimiter delimiter = new CustomDelimiter();
+        String[] result = delimiter.separate("//asdf\n1,2:3asdf4");
+        String customDelimiter = result[0];
+        String numbers = result[1];
+        Assertions.assertThat(customDelimiter).isEqualTo("asdf");
+        Assertions.assertThat(numbers).isEqualTo("1,2:3asdf4");
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자가 슬래시")
+    void add_with_slash_delimiter() {
+        CustomDelimiter delimiter = new CustomDelimiter();
+        String[] result = delimiter.separate("///\n1,2:3/4");
+        String customDelimiter = result[0];
+        String numbers = result[1];
+        Assertions.assertThat(customDelimiter).isEqualTo("/");
+        Assertions.assertThat(numbers).isEqualTo("1,2:3/4");
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자가 역슬래시")
+    void add_with_backslash_delimiter() {
+        CustomDelimiter delimiter = new CustomDelimiter();
+        String[] result = delimiter.separate("//\\\n1,2:3\\4");
+        String customDelimiter = result[0];
+        String numbers = result[1];
+        Assertions.assertThat(customDelimiter).isEqualTo("\\");
+        Assertions.assertThat(numbers).isEqualTo("1,2:3\\4");
+    }
+
+    @Test
+    @DisplayName("두 개의 슬래시가 아니라 하나의 슬래시로 시작")
+    void start_with_single_slash() {
+        CustomDelimiter delimiter = new CustomDelimiter();
+        assertThrows(IllegalArgumentException.class, () -> {
+            String[] result = delimiter.separate("/asdf\n1,2:3");
+        });
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자가 비어있음")
+    void add_with_blank_delimiter() {
+        CustomDelimiter delimiter = new CustomDelimiter();
+        assertThrows(IllegalArgumentException.class, () -> {
+            String[] result = delimiter.separate("//\n1,2:3");
+        });
+    }
+
+    @Test
+    @DisplayName("슬래시로 시작하지 않는데 \\n가 입력됨")
+    void contain_enter() {
+        CustomDelimiter delimiter = new CustomDelimiter();
+        assertThrows(IllegalArgumentException.class, () -> {
+            String[] result = delimiter.separate("\n1,2:3");
+        });
+    }
+
+    @Test
+    @DisplayName("\\n가 없음")
+    void without_enter() {
+        CustomDelimiter delimiter = new CustomDelimiter();
+        assertThrows(IllegalArgumentException.class, () -> {
+            String[] result = delimiter.separate("//a1,2:3");
+        });
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자로 마침표가 입력된 경우")
+    void dot_delimiter() {
+        CustomDelimiter delimiter = new CustomDelimiter();
+        assertThrows(IllegalArgumentException.class, () -> {
+            String[] result = delimiter.separate("//.\n1,2:3.4");
+        });
+    }
+}
