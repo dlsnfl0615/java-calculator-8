8ac5236d4795469bc72909d31757bb9a86fc321c
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-18 17:38:22 +0900

    style(readExpression): 주석 추가

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -6,6 +6,8 @@ public class InputView {
     public String readExpression() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String firstInput = Console.readLine();
+
+        // 커스텀 구분자를 사용하는 경우
         if (firstInput.contains("/")) {
             String secondInput = Console.readLine();
             return firstInput + "\n" + secondInput;
