e8d105ca32b4a8d71ab60c79526451f9f640a480
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-19 19:00:26 +0900

    refactor: printError 메소드 삭제

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -6,8 +6,4 @@ public class OutputView {
     public void printResult(BigDecimal result) {
         System.out.println("결과 : " + result);
     }
-
-    public void printError(String message) {
-        System.out.println("[ERROR] " + message);
-    }
 }
