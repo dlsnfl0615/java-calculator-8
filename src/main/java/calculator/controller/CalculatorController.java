0b74743f1bb7a93bf9327dd0948924a7f1144c4a
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-19 16:08:26 +0900

    fix(run): Exception 발생 후 프로그램 종료

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -17,19 +17,13 @@ public class CalculatorController {
     }
 
     public void run() {
-        try {
-            // View를 통해 사용자 입력 받기
-            String expression = inputView.readExpression();
+        // View를 통해 사용자 입력 받기
+        String expression = inputView.readExpression();
 
-            // Model에게 계산 요청하기
-            BigDecimal result = calculator.add(expression);
+        // Model에게 계산 요청하기
+        BigDecimal result = calculator.add(expression);
 
-            // View를 통해 결과 출력하기
-            outputView.printResult(result);
-
-        } catch (IllegalArgumentException e) {
-            // Model에서 예외 발생 시 View를 통해 에러 메시지 출력하기
-            outputView.printError(e.getMessage());
-        }
+        // View를 통해 결과 출력하기
+        outputView.printResult(result);
     }
 }
