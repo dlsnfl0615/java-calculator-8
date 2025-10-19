d6a37f1a7555d7dae227d633b281919cb1db10a4
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-19 16:10:48 +0900

    fix(readExpression): 엔터키 대신 이스케이프 문자로 수정

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -5,14 +5,6 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     public String readExpression() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String firstInput = Console.readLine();
-
-        // 커스텀 구분자를 사용하는 경우
-        if (firstInput.contains("/")) {
-            String secondInput = Console.readLine();
-            return firstInput + "\n" + secondInput;
-        }
-
-        return firstInput;
+        return Console.readLine();
     }
 }
